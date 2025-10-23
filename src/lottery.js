f7ffe493c7d5652f3ccc2b66378af495b9dab1c7
yurifws <yurifws@gmail.com>
2025-10-23 19:59:40 -0300

    <feat ES-101> rendering contract data

--- src/lottery.js
+++ src/lottery.js
@@ -1,44 +1,73 @@
 import web3 from './web3';
 
-const address = '0xEa1b8a5309d6066Af41F9f0492b247caFd9eB1C5';
-
+const address = '0x8320548419373cF822497f1e72c81261bAf2a150';
 const abi = [
-  { inputs: [], stateMutability: 'nonpayable', type: 'constructor' },
   {
-    inputs: [],
-    name: 'enter',
-    outputs: [],
-    stateMutability: 'payable',
-    type: 'function'
+    "inputs": [],
+    "stateMutability": "nonpayable",
+    "type": "constructor"
   },
   {
-    inputs: [],
-    name: 'getPlayers',
-    outputs: [ [Object] ],
-    stateMutability: 'view',
-    type: 'function'
+    "inputs": [],
+    "name": "enter",
+    "outputs": [],
+    "stateMutability": "payable",
+    "type": "function"
   },
   {
-    inputs: [],
-    name: 'manager',
-    outputs: [ [Object] ],
-    stateMutability: 'view',
-    type: 'function'
+    "inputs": [],
+    "name": "getPlayers",
+    "outputs": [
+      {
+        "internalType": "address[]",
+        "name": "",
+        "type": "address[]"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
   },
   {
-    inputs: [],
-    name: 'pickWinner',
-    outputs: [],
-    stateMutability: 'nonpayable',
-    type: 'function'
+    "inputs": [],
+    "name": "manager",
+    "outputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
   },
   {
-    inputs: [ [Object] ],
-    name: 'players',
-    outputs: [ [Object] ],
-    stateMutability: 'view',
-    type: 'function'
+    "inputs": [],
+    "name": "pickWinner",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "name": "players",
+    "outputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
   }
 ];
 
-export default new web3.eth.Contract(abi, address);
+const lottery = new web3.eth.Contract(abi, address);
+
+export default lottery;

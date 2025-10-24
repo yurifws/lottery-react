36457ed7db1b1eda573d8229eef66e5ebcfebf29
yurifws <yurifws@gmail.com>
2025-10-24 17:41:12 -0300

    <feat ES-105> form setup

--- src/App.js
+++ src/App.js
@@ -23,6 +23,18 @@ class App extends React.Component {
     this.setState( { manager, players, balance } );
   }
 
+  onSubmit = async (event) => {
+    event.preventDefault();
+
+    const accounts = await web3.eth.getAccounts();
+
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei(this.state.value, 'ether')
+    });
+
+  };
+
   render() {
     return (
       <div>
@@ -32,7 +44,7 @@ class App extends React.Component {
           competing to win {web3.utils.fromWei(this.state.balance, 'ether')} ether!
         </p>
         <hr/>
-        <form>
+        <form onSubmit={this.onSubmit}>
           <h4>Want to try your luck?</h4>
           <div>
             <label>Amount of ether to enter</label>
